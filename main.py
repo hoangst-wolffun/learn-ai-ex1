3553fed8a4fcb8430ef627ec21d6f1034ce855fe
hoangst-wolffun <hoang@wolffungame.com>
2023-04-29 14:34:32 +0700

    failed. doi sang dijkstra

--- main.py
+++ main.py
@@ -26,7 +26,7 @@ with open("word_dict_2first_letter.json", "r") as f:
 with open("word_dict_2last_letter.json", "r") as f:
     word_dict_2last_letter = json.load(f)
 
-begin = "ac"
+begin = "al"
 if begin in word_dict_2first_letter:
     listWordBegins = word_dict_2first_letter[begin]
     print(listWordBegins)
@@ -34,7 +34,7 @@ else:
     print("Dont have chain because begin not exit")
     exit()
 
-end = "my"
+end = "bl"
 if end in word_dict_2last_letter:
     listWordEnd = word_dict_2last_letter[end]
     print(listWordEnd)
@@ -42,24 +42,27 @@ else:
     print("Dont have chain because end not exit")
     exit()
 
-arrWordHandled = []
+smallestChain = []
+arr2 = []
 
 
-def find_word(begin_letter, end_letter, my_dict={}, arr=None, arr2=None):
-    global arrWordHandled
+def find_word(begin_letter, end_letter, my_dict=None, arr=None):
+    if my_dict is None:
+        my_dict = {}
+    global smallestChain
+    global arr2
     if arr is None:
         arr = []
-    if arr2 is None:
-        arr2 = []
+
     arr.append(begin_letter)
-    arrWordHandled.append(begin_letter)
+
     print("find_word ", begin, " end = ", end_letter, " arr = ", arr, "arr2 = ", arr2)
-    if len(arr2) > 0:
-        return arr2
+
     if begin_letter not in my_dict:
         print("dont have key begin ", begin_letter)
-        # arr2.append(arr)
-        return arr2
+        arr2.append(arr)
+        if len(arr2) >= 2:
+            return arr2
     else:
         # danh sách các từ bắt đầu với begin
         list_word_begin = word_dict_2first_2last_letter[begin]
@@ -67,14 +70,21 @@ def find_word(begin_letter, end_letter, my_dict={}, arr=None, arr2=None):
             # khớp rồi
             arr.append(end_letter)
             arr2.append(arr)
+            if len(smallestChain) == 0 or len(smallestChain) < len(arr):
+                smallestChain = arr.copy()
+                return
+
+            if len(arr2) > 1:
+                return
             return arr2
         else:
             new_dict = dict(my_dict)
             del new_dict[begin_letter]
-            for new_begin in list_word_begin:
-                if new_begin not in arrWordHandled:
-                    new_arr = arr.copy()
-                    find_word(new_begin, end_letter, new_dict, new_arr, arr2)
+            if len(arr) < 2:
+                for new_begin in list_word_begin:
+                    if new_begin not in arr:
+                        new_arr = arr.copy()
+                        find_word(new_begin, end_letter, new_dict, new_arr)
 
     # print("find_word with begin is", begin_letter, " end is ", end_letter, " arr is ", arr)
     # arr.append(begin_letter)
@@ -104,16 +114,15 @@ def find_word(begin_letter, end_letter, my_dict={}, arr=None, arr2=None):
     #         return arr2
 
 
-list_key_value = find_word(begin, end, word_dict_2first_2last_letter)
+find_word(begin, end, word_dict_2first_2last_letter)
 
-if list_key_value is not None:
-    print(list_key_value)
-    for list_begin in list_key_value:
-        for i in range(len(list_begin) - 1):
-            arr_word_begin = word_dict_2first_letter[list_begin[i]]
-            for word in arr_word_begin:
-                if word[-2:] == list_begin[i + 1]:
-                    print(word)
-else:
-    print("dont have chain")
+# for list_begin in arr2:
+#     for i in range(len(list_begin) - 1):
+#         arr_word_begin = word_dict_2first_letter[list_begin[i]]
+#         for word in arr_word_begin:
+#             if word[-2:] == list_begin[i + 1]:
+#                 print(word)
+#                 break
 
+print("arr2 = ", arr2)
+print("smallestChain = ", smallestChain)
