149fc3c8fa4d3f3985bae4a778fc7e79779e629d
hoangst-wolffun <hoang@wolffungame.com>
2023-04-29 12:13:41 +0700

    failed again

--- main.py
+++ main.py
@@ -25,9 +25,11 @@ begin = "en"
 end = "pa"
 
 
-def find_word(begin_letter, end_letter, arr=None):
+def find_word(begin_letter, end_letter, list_old_ends=[], arr=None, arr2=None):
     if arr is None:
         arr = []
+    if arr2 is None:
+        arr2 = []
     print("find_word with begin is", begin_letter, " end is ", end_letter, " arr is ", arr)
     arr.append(begin_letter)
     if begin_letter in word_dict_2first_2last_letter:
@@ -36,16 +38,26 @@ def find_word(begin_letter, end_letter, arr=None):
         if end_letter in list_ends:
             print("find success - ", end_letter, " with list end ", list_ends)
             arr.append(end_letter)
-            return arr
+            arr2.append(arr)
+            if len(list_old_ends) > 0:
+                new_begin = list_old_ends[0]
+                return find_word(new_begin, end_letter, list_old_ends[1:], arr, arr2)
+            else:
+                return arr2
         else:
             for new_end in list_ends:
                 if new_end not in arr:
-                    return find_word(new_end, end_letter, arr)
+                    list_old_ends_new = word_dict_2first_2last_letter[new_end]
+                    return find_word(new_end, end_letter, list_old_ends_new, arr, arr2)
     else:
         print("dont have the key in dic ", begin_letter)
-        #return arr
+        if len(list_old_ends) > 0:
+            new_begin = list_old_ends[0]
+            return find_word(new_begin, end_letter, list_old_ends[1:], arr, arr2)
+        else:
+            return arr2
 
 
-list_key_value = find_word(begin, end)
+list_key_value = find_word(begin, end, word_dict_2first_2last_letter[begin])
 
 print(list_key_value)
